51339017fc27d9d4c36028acb23e8fccb67fb54d
dan <danielgomezrico@gmail.com>
2024-11-21 12:13:06 -0500

    Add implementations

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -2,9 +2,9 @@ class Song {
   final String title;
   final String artist;
   final String duration;
-  final String imageUrl; // URL to the song image.
+  final String imageUrl;
 
-  Song({
+  const Song({
     required this.title,
     required this.artist,
     required this.duration,
