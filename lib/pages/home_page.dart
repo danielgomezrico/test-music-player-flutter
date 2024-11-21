23bb27b83139e91b39a94e7be1f0a122879b29e0
dan <danielgomezrico@gmail.com>
2024-11-21 12:16:53 -0500

    Remove the need of shrink wrap

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,58 +14,50 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Playlist'),
       ),
-      body: Column(
-        children: [
-          ListView.builder(
-            itemCount: musicBloc.state.songs.length,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              final song = songs[index];
-              final isSelected = musicBloc.state.currentSongIndex == index;
+      body: ListView.builder(
+        itemCount: musicBloc.state.songs.length,
+        itemBuilder: (context, index) {
+          final song = songs[index];
+          final isSelected = musicBloc.state.currentSongIndex == index;
 
-              return ListTile(
-                selected: isSelected,
-                title: Text(song.title),
-                subtitle: Text(song.artist),
-              );
-            },
-          ),
-          const Spacer(),
-          // Control bar with Play, Previous, and Next buttons.
-          Container(
-            color: Colors.grey[200],
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Previous button.
-                IconButton(
-                  icon: const Icon(Icons.skip_previous),
-                  onPressed: () {
-                    musicBloc.add(PreviousSongEvent());
-                  },
-                ),
-                // Play/Pause button.
-                IconButton(
-                  icon: musicBloc.state.isPlaying
-                      ? const Icon(Icons.pause)
-                      : const Icon(Icons.play_arrow),
-                  onPressed: () {
-                    musicBloc.add(const PlayPauseEvent());
-                  },
-                ),
-                // Next button.
-                IconButton(
-                  icon: const Icon(Icons.skip_next),
-                  onPressed: () {
-                    musicBloc.add(NextSongEvent());
-                  },
-                ),
-              ],
+          return ListTile(
+            selected: isSelected,
+            title: Text(song.title),
+            subtitle: Text(song.artist),
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.grey[200],
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Previous button.
+            IconButton(
+              icon: const Icon(Icons.skip_previous),
+              onPressed: () {
+                musicBloc.add(PreviousSongEvent());
+              },
             ),
-          ),
-        ],
+            // Play/Pause button.
+            IconButton(
+              icon: musicBloc.state.isPlaying
+                  ? const Icon(Icons.pause)
+                  : const Icon(Icons.play_arrow),
+              onPressed: () {
+                musicBloc.add(const PlayPauseEvent());
+              },
+            ),
+            // Next button.
+            IconButton(
+              icon: const Icon(Icons.skip_next),
+              onPressed: () {
+                musicBloc.add(NextSongEvent());
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
